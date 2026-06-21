8c7abede3a11db9064688cd2f74ee1ab521f68cc
Izge Bayyurt <izgebayyurt@gmail.com>
2026-06-21 19:24:13 +0000

    Update Glass Fold: community sorting + moderation

--- glassfold/assets/web-BJe9cM0h.js
+++
@@ -1,1 +0,0 @@
-import{y as n}from"./index-B1PsD6qy.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ glassfold/assets/web-C5xtEAtG.js
@@ -0,0 +1,1 @@
+import{y as n}from"./index-BOLjWYun.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
