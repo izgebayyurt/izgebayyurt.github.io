c9ec30e40d8e744cd4fe10ab8988b241f3f69e66
Izge Bayyurt <izgebayyurt@gmail.com>
2026-06-21 05:53:27 +0000

    Update Glass Fold: every mechanic now purposeful (389 levels)

--- glassfold/assets/web-C40J1J7E.js
+++
@@ -1,1 +0,0 @@
-import{W as n}from"./index-DnB_0NTR.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ glassfold/assets/web-uXvoqo3_.js
@@ -0,0 +1,1 @@
+import{W as n}from"./index-Cd7mgMkn.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
