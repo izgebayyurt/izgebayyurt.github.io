f2391c1459e25758776fa705923ec2c48ae52f0c
izgebayyurt <izgebayyurt@gmail.com>
2026-06-22 13:27:41 +0000

    Update Glass Fold: drop interstitials

--- glassfold/assets/web-C8bTGgTd.js
+++
@@ -1,1 +0,0 @@
-import{y as n}from"./index-h9OcvUwc.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ glassfold/assets/web-DVqMfrYH.js
@@ -0,0 +1,1 @@
+import{y as n}from"./index-C2bjqVIb.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
