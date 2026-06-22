a25a28f20827ffa3ffd7fa3ca1cc9868b1be247e
izgebayyurt <izgebayyurt@gmail.com>
2026-06-22 08:50:52 +0000

    Update Glass Fold: chisel icon, privacy policy, reduced-motion

---
+++ glassfold/assets/web-C8bTGgTd.js
@@ -0,0 +1,1 @@
+import{y as n}from"./index-h9OcvUwc.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
--- glassfold/assets/web-DFbCvhFT.js
+++
@@ -1,1 +0,0 @@
-import{y as n}from"./index-BK0U0RVk.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
