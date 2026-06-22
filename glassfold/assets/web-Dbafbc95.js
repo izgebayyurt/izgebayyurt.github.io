294101df3e305cbba81f219d3f1719546775cc3d
izgebayyurt <izgebayyurt@gmail.com>
2026-06-22 16:56:09 +0000

    Update Glass Fold: 2x in daily box, free coins to shop

--- glassfold/assets/web-Dbafbc95.js
+++
@@ -1,1 +0,0 @@
-import{y as n}from"./index-CMDp54GI.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ glassfold/assets/web-Duh3kvXv.js
@@ -0,0 +1,1 @@
+import{y as n}from"./index-COz2gUbR.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
