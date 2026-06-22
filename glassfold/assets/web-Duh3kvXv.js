723f04d09ac7eef31b08bbc14fe24cfdf25f51a7
izgebayyurt <izgebayyurt@gmail.com>
2026-06-22 17:29:46 +0000

    Update Glass Fold: square 2x button + single-dash copy

--- glassfold/assets/web-Duh3kvXv.js
+++
@@ -1,1 +0,0 @@
-import{y as n}from"./index-COz2gUbR.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
---
+++ glassfold/assets/web-S9_SbUpV.js
@@ -0,0 +1,1 @@
+import{y as n}from"./index-eDG33b1E.js";class r extends n{async show(e){}async hide(e){}}export{r as SplashScreenWeb};
